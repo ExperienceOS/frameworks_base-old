6972d56beba9503f1267cbfe7a05cce619543d7e
TTTT55 <iamtttt55@gmail.com>
2021-06-26 04:00:49 +0000

    Revert "BrightnessController: tune the slider animation duration"
    This reverts commit 3e8f2b97d65ace27a4bd9f7492ba1203f1258f6b.

--- packages/SystemUI/src/com/android/systemui/settings/BrightnessController.java
+++ packages/SystemUI/src/com/android/systemui/settings/BrightnessController.java
@@ -23,7 +23,6 @@ import static com.android.settingslib.display.BrightnessUtils.convertLinearToGam
 import android.animation.ValueAnimator;
 import android.content.ContentResolver;
 import android.content.Context;
-import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.hardware.display.DisplayManager;
 import android.net.Uri;
@@ -109,8 +108,6 @@ public class BrightnessController implements ToggleSlider.Listener {
     private boolean mControlValueInitialized;
 
     private ValueAnimator mSliderAnimator;
-    private static final float mDefaultBrightnessRampRateSlow = 0.2352941f;
-    private final float mBrightnessRampRateSlow;
 
     public interface BrightnessStateChangeCallback {
         public void onBrightnessLevelChanged();
@@ -538,13 +535,6 @@ public class BrightnessController implements ToggleSlider.Listener {
         final long animationDuration = SLIDER_ANIMATION_DURATION * Math.abs(
                 mControl.getValue() - target) / GAMMA_SPACE_MAX;
         mSliderAnimator.setDuration(animationDuration);
-        // Only override the duration scale when the ramp rate is different from the default value
-        if (mBrightnessRampRateSlow > 0 &&
-                    Math.abs(mBrightnessRampRateSlow - mDefaultBrightnessRampRateSlow) > 0.01f) {
-            float durationScale = Math.min(
-                    mDefaultBrightnessRampRateSlow / mBrightnessRampRateSlow, 3.0f);
-            mSliderAnimator.overrideDurationScale(durationScale);
-        }
         mSliderAnimator.start();
     }
 
