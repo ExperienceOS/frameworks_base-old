AOSPA/android_frameworks_base-old

SystemUI: Query IMS state after CapabilityCallback is regisered
Query IMS state after CapabilityCallback is regisered
CRs-Fixed: 2364714

Change-Id: I9a527840ab14844f683a5a86602b54ce787581f4

packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -380,6 +380,19 @@ public class MobileSignalController extends SignalController<
         } catch (ImsException e) {
             Log.d(mTag, "unable to addCapabilitiesCallback callback.");
         }
+        queryImsState();
+    }
+
+    private void queryImsState() {
+        TelephonyManager tm = mPhone.createForSubscriptionId(mSubscriptionInfo.getSubscriptionId());
+        boolean isVoiceCapable = tm.isVolteAvailable();
+        boolean isVideoCapable = tm.isVideoTelephonyAvailable();
+        Log.d(mTag, "tm=" + tm +" phone=" + mPhone
+                + " isVoiceCapable=" + isVoiceCapable
+                + " isVideoCapable=" +isVideoCapable);
+        if ( isVoiceCapable || isVideoCapable ) {
+            mCurrentState.showHD = true;
+        }
     }
 
     private void removeListeners() {

ExperienceOS/frameworks_base-old

packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -412,6 +412,19 @@ public class MobileSignalController extends SignalController<
         } catch (ImsException e) {
             Log.d(mTag, "unable to addCapabilitiesCallback callback.");
         }
+        queryImsState();
+    }
+
+    private void queryImsState() {
+        TelephonyManager tm = mPhone.createForSubscriptionId(mSubscriptionInfo.getSubscriptionId());
+        boolean isVoiceCapable = tm.isVolteAvailable();
+        boolean isVideoCapable = tm.isVideoTelephonyAvailable();
+        Log.d(mTag, "tm=" + tm +" phone=" + mPhone
+                + " isVoiceCapable=" + isVoiceCapable
+                + " isVideoCapable=" +isVideoCapable);
+        if ( isVoiceCapable || isVideoCapable ) {
+            mCurrentState.isVolteRegistered = true;
+        }
     }
 
     private void removeListeners() {